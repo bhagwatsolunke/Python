37f043afe9e00ae5277548a2bcb433504c9896be
bhagwatsolunke <bhagwatsolunke007@gmail.com>
2023-06-01 17:14:11 +0530

    Python mini projects

---
+++ quiz_game.py
@@ -0,0 +1,36 @@
+print("Welcome to my computer quiz!")
+playing =input ("Do you want to play? ")
+if playing.lower() !="yes":
+    quit()
+print("Okey! let's play :)")
+score=0
+answer = input("what does cpu stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score+=1
+else: 
+    print("Incorrect")
+
+answer = input("what does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score+=1
+else: 
+    print("Incorrect")
+
+answer = input("what does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score+=1
+else: 
+    print("Incorrect")    
+
+answer = input("what does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score+=1
+else: 
+    print("Incorrect")
+
+print("You got " + str(score)+ " question correct!")
+print("You got " + str((score / 4 )*100)+  " %.")
